1f8f87f81fd11391ff3179896243e1186de2a004
jacoobanderson <j00cwa@gmail.com>
2022-09-17 18:09:17 +0200

    Create menu and class structure

--- interfaceCreator/src/index.js
+++ interfaceCreator/src/index.js
@@ -1,8 +1,10 @@
 import { InterfaceCreator } from "./interface-creator.js";
 
 const ui = new InterfaceCreator()
-ui.createMenu({
+ui.setMenu({
     1: 'Add scores',
     2: 'Calculate sum',
     3: 'Calculate average'
 })
+
+ui.start()
--- interfaceCreator/src/interface-creator.js
+++ interfaceCreator/src/interface-creator.js
@@ -1,8 +1,30 @@
 export class InterfaceCreator {
-    // private name?
-    createMenu(menuOptions) {
-        for (const [key, value] of Object.entries(menuOptions)) {
-            console.log(key + '. ' + value + '.')
-        }
+  #menu
+  // private name?
+  // menuColor
+  // introColor
+
+  start() {
+    this.#createMenu()
+  }
+
+  #createMenu() {
+    for (const [key, value] of Object.entries(this.#menu)) {
+      console.log(key + ". " + value + ".")
     }
+  }
+
+  setMenu(menuOptions) {
+    this.#menu = menuOptions
+  }
+  // getInput() {
+
+  // }
+
+  //method that gets input
+  //intromessage
+  //getusername
+  //exit
+  //setcolor
+  //submenus?
 }
